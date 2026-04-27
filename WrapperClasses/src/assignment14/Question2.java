cc41a1b4967b95c630652fd7b8628c6512c8e0b1
Anjali Yadav <anjaliofficial0101@gmail.com>
2026-04-27 23:14:40 +0530

    Assignment-14

---
+++ WrapperClasses/src/assignment14/Question2.java
@@ -0,0 +1,19 @@
+package assignment14;
+public class Question2 {
+        public void wrapperToPrimitive(){
+            double num1=33.33;
+            double num2=33.33;
+
+            int num3=(int)num2;
+
+            System.out.println("Double object "+num1);
+            System.out.println("Double primitive "+num2);
+            System.out.println("Double primitive "+num3);
+        }
+
+        static void main(String[] args){
+            Question2 q1=new Question2();
+            q1.wrapperToPrimitive();
+        }
+    }
+
---
+++ WrapperClasses/src/assignment14/Question3.java
@@ -0,0 +1,23 @@
+package assignment14;
+
+public class Question3 {
+    public void autoUnBoxing(){
+        List<Integer>list=newArray List<>();
+        List.addd(1);
+        List.addd(2);
+        List.addd(3);
+        List.addd(4);
+        List.addd(5);
+        System.out.println("List:-" +List);
+        int sum =0 ;
+        for(Integer num: List){
+                    sum+=num;
+                }
+                System.out.println("sum of list: "+sum);
+
+            }
+            static void main(String[] args){
+                Question3 q3=new Question3();
+                q3.autoBoxing();
+            }
+        }
